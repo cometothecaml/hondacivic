671a3464c37ffa1fbc17db891d0034094045f32a
cometothecaml <50890447+cometothecaml@users.noreply.github.com>
2020-09-11 15:31:38 -0700

    added icon

--- cringe.js
+++ cringe.js
@@ -3,15 +3,23 @@ element2.hidden=true
 var element3 = document.getElementById("sellbutton")
 var element = document.getElementById("con")
 element3.hidden=true
-var disconnect = true
 if (typeof(window.ethereum) === "undefined"){
     nometamaskwarn()
+}else{
+    ethereum.on('accountsChanged', function (accounts) {
+        if (accounts[0]===undefined){
+            disconnected()
+        }
+        else{
+            updateBalance(accounts[0])
+        }
+    })
 }
 document.getElementById("con").addEventListener("click", function(){console.log("connect attempt"); attemptConnect();});
 async function attemptConnect(){
     try{
         ethereum.request({ method: 'eth_requestAccounts' }).then(function(res){updateBalance(res[0]);})
-
+        
     }
     catch(error){
         document.getElementById("wallet").innerHTML="Connection fail, try again"
@@ -34,23 +42,9 @@ function updateBalance(add){
     element2.hidden=false
     element3.hidden=false
     document.getElementById("wallet").innerHTML="Wallet connected: "+add
-    disconnect = false
-    ethereum.on('accountsChanged', function (accounts) {
-    try{
-        if (!disconnect){
-        ethereum.request({ method: 'eth_requestAccounts' }).then(function(res){updateBalance(res[0]);})
-        }
-    }
-    catch(error){
-            disconnected()
-        }
-    })
-    ethereum.on('disconnect', function (accounts) {
-        disconnected()
-    })
+
 }
 function disconnected(){
-    disconnect = true
     element.hidden = false
     element2.hidden = true
     element3.hidden = true
