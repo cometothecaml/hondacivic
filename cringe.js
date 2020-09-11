a9c31c244b5e71a67b7a92aebc93732e98d455c8
cometothecaml <50890447+cometothecaml@users.noreply.github.com>
2020-09-11 14:12:54 -0700

    update

--- cringe.js
+++ cringe.js
@@ -1,11 +1,27 @@
 var element2 = document.getElementById("buybutton")
 element2.hidden=true
 var element3 = document.getElementById("sellbutton")
+var element = document.getElementById("con")
 element3.hidden=true
 if (typeof(window.ethereum) === "undefined"){
-    var element = document.getElementById("con")
+    nometamaskwarn()
+}
+if (typeof(window.ethereum) !== "undefined"&& window.ethereum.isConnected === true){
+    walletConnected()
+}
+document.getElementById("con").addEventListener("click", function(){console.log("connect attempt"); attemptConnect();});
+async function attemptConnect(){
+    try{
+        await(window.ethereum.enable())
+        walletConnected()
+
+    }
+    catch(error){
+        document.getElementById("wallet").innerHTML="Connection fail, try again"
+    }
+}
+function nometamaskwarn(){
     element.hidden=true
-    
     const newDiv = document.createElement("div")
     var newContent = document.createElement("p")
     newContent.innerHTML="Please install Metamask to continue. Reload when done"
@@ -15,26 +31,15 @@ if (typeof(window.ethereum) === "undefined"){
     document.body.insertBefore(newDiv, document.getElementById("buyButton"))
     console.log("Eth wallet not detected")
 }
-if (typeof(window.ethereum) !== "undefined"&& window.ethereum.isConnected === true){
-    var element = document.getElementById("con")
+function walletConnected(){
     element.hidden=true
     element2.hidden=false
     element3.hidden=false
     document.getElementById("wallet").innerHTML="Wallet connected: "+window.ethereum.selectedAddress
+    setInterval(function(){if (!window.ethereum.isConnected){disconnected();}},100)
 }
-document.getElementById("con").addEventListener("click", function(){console.log("connect attempt"); attemptConnect();});
-async function attemptConnect(){
-    try{
-        await(window.ethereum.enable())
-        var element = document.getElementById("con")
-        element.hidden=true
-        element2.hidden=false
-        element3.hidden=false
-        document.getElementById("wallet").innerHTML="Wallet connected: "+window.ethereum.selectedAddress
-
-    }
-    catch(error){
-        document.getElementById("wallet").innerHTML="Connection fail, try again"
-
-    }
+function disconnected(){
+    element.hidden = false
+    element2.hidden = true
+    element3.hidden = true
 }
